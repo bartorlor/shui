d2971b493f1f67e9fa058717414ea2d5a1430bba
Ronald <Ronald@gmail.com>
2018-11-29 15:15:52 -0800

    save all data

--- client/src/components/Txns.vue
+++ client/src/components/Txns.vue
@@ -10,10 +10,11 @@
       <div v-for="txn of txns" class="txn-item">
         <!-- <a @click="id = txn._id">{{ txn.title }}</a> -->
         <router-link :to="{name: 'txn', params: { id: txn._id }}">{{ txn.symbol}}</router-link>
-        <span class="badge">{{ txn.symbol}}</span>
-        <span class="badge">{{ txn.description}}</span>
-        <span class="badge">{{ txn.action}}</span>
         <span class="date">{{ txn.stlmtDate | date }}</span>
+        <span class="badge">{{ txn.action}}</span>
+        <span class="badge">{{ txn.qty}}</span>
+        <span class="badge">{{ txn.price}}</span>
+        <span class="badge">{{ txn.amt}}</span>
       </div>
     </section>
 
