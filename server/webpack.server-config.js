92f5457c9931aa4ac9ea7ed9ce109c7ee16a031d
tesla <teslaurban@g.com>
2019-10-17 22:49:44 -0700

    now build server works on npm run start, build is good too npm run build

--- server/webpack.server-config.js
+++ server/webpack.server-config.js
@@ -1,38 +1,38 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
 const webpack = require('webpack');
 
 module.exports = {
+  mode: 'development',
+  entry: { server: ['./src/index.js'] },
   target: 'node',
-  entry: ['./src/index.js', './node_modules/webpack/hot/poll?1000'],
+  externals: [nodeExternals()],
   output: {
-    path: './dist',
     filename: 'server.bundle.js',
-    libraryTarget: 'commonjs',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
   },
-  resolve: {
-    extensions: ['', '.js', '.jsx'],
-  },
-  externals: [/^[a-z]/],
   module: {
-    loaders: [
+    rules: [
       {
-        test: /\.jsx$/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', '@babel/preset-env'],
-        },
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['@babel/preset-env'],
+        test: /\.js?$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', {
+                targets: { node: '8' },
+              }],
+            ],
+          },
         },
       },
     ],
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      __isBrowser__: 'false',
+    }),
   ],
   devtool: 'source-map',
 };
