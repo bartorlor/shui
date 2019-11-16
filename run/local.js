403cf567146a27298c3eda817d9c6ee70112c643
tesla <teslaurban@g.com>
2019-11-15 22:10:46 -0800

    auto login

--- run/local.js
+++ run/local.js
@@ -14,14 +14,14 @@ console.log('22');
 console.log('22');
     page = await browser.newPage();
     page.on('console', msg => console.log('PAGE LOG:', msg.text()));
-    page.setCacheEnableed([false]);
+    page.setCacheEnabled([false]);
     await page.setViewport({
       width: 640,
       height: 480,
       deviceScaleFactor: 1,
     });
     console.log(`Msg 1 ${page}`);
-  } catch (er) {
+  } catch (error) {
     console.log(`Msg Error; ${error.stack}`);
     console.log(`Msg Error; ${error.message}`);
   }
@@ -32,17 +32,13 @@ async function login() {
     await page.goto('http://localhost:3000');
     await page.waitFor(1000);
     await page.click('a[href$="login"]');
-    console.log(`Msg 1 ${page}`);
     await page.waitFor(1000);
     let el = await page.$('input[name=email]');
     await el.type('test@g.ca', { delay: 100 });
-    console.log(`Msg 1 ${page}`);
     // await el.press('Enter');
-    el = await page.$('input[name=email]');
+    el = await page.$('input[name=password]');
     await el.type('password', { delay: 100 });
-    console.log(`Msg 1 ${page}`);
     await page.click('button[type=submit]');
-    console.log(`Msg 1 ${page}`);
   } catch (er) {
     console.log(`Msg Error; ${error.stack}`);
     console.log(`Msg Error; ${error.message}`);
@@ -53,24 +49,19 @@ async function signin() {
   try {
     await page.goto('http://localhost:3000');
     await page.waitFor(2000);
-    console.log(`Msg 1 ${page}`);
     await page.click('a[href$="login"]');
-    await page.waitFor(2000);
+    await page.waitFor(1000);
     await page.click('button[type=button]'); //signin
-    console.log(`Msg 1 ${page}`);
-    await page.waitFor(2000);
+    await page.waitFor(300);
     let el = await page.$('input[name=email]');
-    console.log(`Msg 1 ${page}`);
     await el.type('test@g.ca', { delay: 100 });
     await el.press('Enter');
-    console.log(`Msg 1 ${page}`);
     el = await page.$('input[name=password]');
     await el.type('password', { delay: 100 });
     el = await page.$('input[name=verify-password]');
-    console.log(`Msg 1 ${page}`);
     await el.type('password', { delay: 100 });
     await page.click('button[type=submit]');
-  } catch (er) {
+  } catch (error) {
     console.log(`Msg Error; ${error.stack}`);
     console.log(`Msg Error; ${error.message}`);
   }
@@ -79,7 +70,7 @@ async function signin() {
 
 async function main(){
   await init();
-  // await login();
-  await signin();
+  await login();
+  // await signin();
 }
 main();
